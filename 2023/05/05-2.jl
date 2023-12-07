d8cb38af1eac96d6b661d9ff6fd7c72978f2901c
Tobias Jessen <mail@tobijes.com>
2023-12-07 22:57:40 +0100

    Day 5 part 2. Sample working. Not full

--- 2023/05/05-2.jl
+++ 2023/05/05-2.jl
@@ -3,8 +3,9 @@ using DataStructures
 
 struct MapRange
     sourceStart::Int
+    sourceStop::Int
     destinationStart::Int
-    size::Int
+    destinationStop::Int
 end
 
 struct Range
@@ -18,12 +19,16 @@ function cleansplit(s::AbstractString, sep::String)
     map(x->strip(x), splitted)
 end
 
+function validrange(start, stop)
+    return stop - start >= 0
+end
+
 function work(filepath::String)
     f = open(filepath)
 
     l = readline(f)
     seeds = parse.(Int, cleansplit(replace(l, "seeds: " => ""), " "))
-    ranges = [Range(seeds[i], seeds[i]+seeds[i+1]-1, 0) for i in 1:2:length(seeds) ]
+    ranges = [Range(seeds[i], seeds[i]+seeds[i+1]-1, 1) for i in 1:2:length(seeds) ]
 
     names = []
     maps = []
@@ -39,8 +44,8 @@ function work(filepath::String)
             continue
         end
         
-        numbers = parse.(Int, cleansplit(l, " "))
-        mapRange = MapRange(numbers[2], numbers[1], numbers[3])
+        destinationStart, sourceStart, size = parse.(Int, cleansplit(l, " "))
+        mapRange = MapRange(sourceStart, sourceStart + size-1, destinationStart, destinationStart + size-1)
         push!(maps[end], mapRange)
     end
 
@@ -57,7 +62,8 @@ function work(filepath::String)
         end
         println()
     end
-
+    
+    locations = []
     queue = Queue{Range}()
     for range in ranges
         enqueue!(queue, range)
@@ -66,34 +72,57 @@ function work(filepath::String)
     while !isempty(queue)
 
         range = dequeue!(queue)
+        println("Dequeued range: ", range)
+        if range.level > length(maps)
+            push!(locations, range.start)
+            continue
+        end
+
         start = range.start
         stop = range.stop
-        map = maps[range.level]
-        for mapranges in 
+        
+        # For each maprange in the current map
+        for maprange in maps[range.level]
 
+            # Part inside 
+            insideStart = max(range.start, maprange.sourceStart)
+            insideStop = min(range.stop, maprange.sourceStop)
+            
+            if validrange(insideStart, insideStop)
+                newStart = maprange.destinationStart + (insideStart - maprange.sourceStart)
+                newStop = maprange.destinationStart + (insideStop - maprange.sourceStart)
+                newRange = Range(newStart, newStop, range.level + 1)
+                println("Enquing inside:, ", newRange)
+                enqueue!(queue, newRange)
+            end
+
+            # Part before
+            beforeStart = range.start
+            beforeStop = insideStart - 1
+
+            if validrange(beforeStart, beforeStop)
+                newRange = Range(beforeStart, beforeStop, range.level + 1)
+                println("Enquing before:, ", newRange)
+                enqueue!(queue, newRange)
+            end
+
+            # Part after
+            start = max(range.start, maprange.sourceStop) + 1
+            stop = range.stop
+
+            println("After Start Stop ", start, " ", stop)
+            if !validrange(start, stop)
+                break
+            end
+        end
+
+        if validrange(start, stop)
+            newRange = Range(start, stop, range.level + 1)
+            println("Enquing after:, ", newRange)
+            enqueue!(queue, newRange)
+        end
     end
     
-
-
-    locations = []
-    for seed in seeds
-        num = seed
-        print("Seed: ", num)
-        for map in maps
-            # println(map)
-            for maprange in map
-                # println(maprange)
-                if maprange.sourceStart <= num < maprange.sourceStart+maprange.size
-                    # println("THis")
-                    num = maprange.destinationStart + (num-maprange.sourceStart)
-                    break
-                end
-            end
-            print(" ", num)
-        end
-        println()
-        push!(locations, num)
-    end
     println(locations)
     minimum(locations)
 end
