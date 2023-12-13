47f103ed335a33ed40bc5e34c21a31fccc7db498
Tobias Jessen <mail@tobijes.com>
2023-12-13 21:59:37 +0100

    Day 9

---
+++ 2023/09/09-2.jl
@@ -0,0 +1,61 @@
+using Test
+
+DEBUG = true
+
+function debug(args...)
+    if DEBUG
+        println(args...)
+    end
+end
+
+function cleansplit(s::AbstractString, sep::String)
+    splitted = split(s, sep, keepempty=false)
+    map(x->strip(x), splitted)
+end
+
+function diff(array)
+    result = zeros(length(array) - 1)
+    for i in 1:length(array)-1
+        result[i] = array[i+1] - array[i]
+    end
+    return result
+end
+
+# function extrapolate(left, below)
+#     push
+# end
+
+function work(filepath::String)
+    f = open(filepath)
+
+    s = 0
+    for l in eachline(f)
+        histories = []
+        history = parse.(Int, cleansplit(l, " "))
+        push!(histories, history)
+        while any(history .!= 0)
+            history = diff(history)
+            push!(histories, history)
+        end
+        for history in histories
+            debug(history)
+        end
+
+        for i in length(histories)-1:-1:1
+            pushfirst!(histories[i], histories[i][begin] - histories[i+1][begin]) # left + below
+        end
+        for history in reverse(histories)
+            debug(history)
+        end
+        s += histories[begin][begin]
+        # break
+    end
+
+    close(f)
+
+    s
+end
+result = work("09/sample.txt")
+DEBUG=false
+result = work("09/input.txt")
+println("Result: ", result)
