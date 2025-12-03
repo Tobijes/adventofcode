2ae3332b07f83b1ddc32fc8042983d8497e335b1
Tobias Jessen <mail@tobijes.com>
2025-12-03 18:02:16 +0100

    Day 2

---
+++ 2025/02/02-2.py
@@ -0,0 +1,58 @@
+# Load data
+
+import sys
+
+is_test = False
+if len(sys.argv) > 1 and sys.argv[1] == "t":
+    is_test = True
+    load_file = "input.txt"
+elif len(sys.argv) > 1:
+    sample_name = sys.argv[1]
+    load_file = "sample_" + sample_name + ".txt"
+else:
+    load_file = "sample.txt"
+
+print(f"{is_test=}", load_file)
+
+with open(load_file) as f:
+    data =  f.readlines()
+        
+data = [l.strip() for l in data]
+
+def print_matrix(matrix):
+    for row in range(len(matrix)):
+        for col in range(len(matrix[0])):
+            print(matrix[row][col], end="")
+        print()
+
+# Problem solution
+ranges = data[0].split(',')
+
+def chunks(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i+n]
+
+def has_repeats(x: int):
+    s = str(x)
+    mid = len(s)//2
+    for n in range(1, mid + 1):
+        chunked = list(chunks(s,n))
+        unique = set(chunked)
+        if len(chunked) >= 2 and len(unique) == 1:
+            # print(s, n, unique)
+            return True
+    return False
+
+
+score = 0
+
+for r in ranges: 
+    first, last = [int(x) for x in r.split('-')]
+    myrange = range(first, last+1,1)
+    # print(first, last, ",".join(map(str,list(myrange))))
+    for x in myrange:
+        if has_repeats(x):
+            score += x
+
+
+print(score)
