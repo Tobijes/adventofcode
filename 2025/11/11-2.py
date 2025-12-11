d6fd4607a1270ae75394bbb0c70510c8ce37c526
Tobias Jessen <mail@tobijes.com>
2025-12-11 23:36:36 +0100

    Generated folders

---
+++ 2025/11/11-2.py
@@ -0,0 +1,28 @@
+# Load data
+
+import sys
+
+is_test = False
+if len(sys.argv) > 1 and sys.argv[1] == "t":
+    is_test = True
+    load_file = "input.txt"
+elif len(sys.argv) > 1:
+    sample_name = sys.argv[1]
+    load_file = "sample_" + sample_name + ".txt"
+else:
+    load_file = "sample.txt"
+
+print(f"{is_test=}", load_file)
+
+with open(load_file) as f:
+    data =  f.readlines()
+        
+data = [l.strip() for l in data]
+
+def print_matrix(matrix):
+    for row in range(len(matrix)):
+        for col in range(len(matrix[0])):
+            print(matrix[row][col], end="")
+        print()
+
+# Problem solution
