e3322367506cb3aaf0cbe06fd48d76db9294869d
Tobias Jessen <mail@tobijes.com>
2025-12-12 15:23:38 +0100

    Day 10 part 1
    Refactor machine parsing and button press logic.

--- 2025/10/10-1.py
+++ 2025/10/10-1.py
@@ -1,8 +1,6 @@
-# Load data
+import re
 
 import sys
-from dataclasses import dataclass
-import re
 
 is_test = False
 if len(sys.argv) > 1 and sys.argv[1] == "t":
@@ -28,91 +26,55 @@ def print_matrix(matrix):
         print()
 
 # Problem solution
-@dataclass
-class Machine:
-    light_pattern_str: str 
-    button_masks: list[int]
-    joltages: list[int]
+def num2bnum(nb: int, length: int) -> int:
+    s = ['0'] * length
+    for n in nb:
+        s[n] = '1'
+    s = "".join(s)
+    return s
 
-def binarypos(k:int, l:int):
-    a = ['0'] * l
-    a[k] = '1'
-    
-    return int("0b" + "".join(a),2)
+def parse_machine(line: str):
+    light = line[line.index('[')+1:line.index(']')]
+    light = ['1' if c == '#' else '0' for c in light]
+    light = "".join(light)
+    buttons_strip = line[line.index('('):line.index('{')-1]
+    buttons_strip = buttons_strip.replace('(', '').replace(')','')
+    buttons_strs  = buttons_strip.split(" ")
+    buttons_strs = [list(map(int, bs.split(","))) for bs in buttons_strs]
+    button_masks = [num2bnum(but, len(light)) for but in buttons_strs]
+    return light,  button_masks
 
-def print_machine(m: Machine):
-    print(f"Light: {m.light_pattern_str}", end=" | Buttons: ")
+def find_button_presses(target, buttons):
 
-    for b in m.button_masks:
-        s = f"{b:b}".zfill(len(m.light_pattern_str))
-        print(s, end=" ")
-    
-    print("")
+    filtermax = 2**len(buttons)
+    filterwidth = len(bin(filtermax)[2:]) - 1
+    # print(f"{filtermax=:b}")
+    solutions = []
+    for i in range(filtermax):
+        binary_str = bin(i)[2:].zfill(filterwidth)
+        # print(binary_str)
+        current = 0
+        for i, b in enumerate(binary_str):
+            if b == '1':
+                current ^= buttons[i]
 
-machines: list[Machine] = []
+        if current == target:
+            solutions.append(binary_str)
 
+    return solutions
+
+score = 0
 for d in data:
-    # printl(d)
-    (m := re.match(r"\[(.+)\] (.*) \{(.+)\}", d))
-    groups = m.groups()
-    # light
-    light_pattern_str = groups[0].replace('.','0').replace('#','1')
-    L = len(light_pattern_str)
-    # button
-    buttons = [list(map(int,g.split(","))) for g in groups[1].replace('(','').replace(')','').split()]
-    button_masks = [sum([binarypos(k,L) for k in button]) for button in buttons]
+    target_str, button_strs = parse_machine(d)
+    print(button_strs, button_strs)
 
-    # joltages
-    joltages = groups[2] 
-    machines.append(Machine(
-        light_pattern_str=light_pattern_str,
-        button_masks=button_masks,
-        joltages=[]
-    ))
+    target = int(target_str, 2)
+    buttons = [int(bs, 2) for bs in button_strs]
 
-for m in machines:
-    print_machine(m)
+    solutions = find_button_presses(target, buttons)
+    print(f"{solutions=}")
+    solution_presses = [sum([1 if c == '1' else 0 for c in sol]) for sol in solutions]
+    score += min(solution_presses)
 
-def toggle(pattern: tuple[bool], indexes:list[int]):
-    lst = list(pattern)
-    for i in indexes:
-        lst[i] = not pattern[i]
-    return tuple(lst)
+print(f"{score=}")
 
-# cache = dict()
-
-# def find_match(pattern: tuple[bool], presses: int, machine: Machine):
-#     if pattern in cache and presses > cache[pattern]:
-#         return cache[pattern]
-    
-#     if presses > 100:
-#         return presses
-    
-#     if pattern == machine.light_pattern:
-#         # print("Match!")
-#         return presses
-    
-#     results = []
-#     for button_idxs in machine.buttons:
-#         new_pattern = toggle(pattern, button_idxs)
-#         # print(pattern, button_idxs, new_pattern)
-#         new_presses = find_match(new_pattern, presses+1, machine)
-
-#         results.append(new_presses)
-    
-#     min_presses = min(results)
-
-#     if pattern not in cache:
-#         cache[pattern] = min_presses
-#     cache[pattern] = min(cache[pattern], min_presses)
-#     return min_presses
-
-# score = 0
-# for i, m in enumerate(machines):
-#     cache = {}
-#     pattern = tuple([False] * len(m.light_pattern))
-#     presses = find_match(pattern, 0, m)
-#     print(f"{i+1}/{len(machines)} {presses=}")
-#     score += presses
-
-# print(f"{score=}")
