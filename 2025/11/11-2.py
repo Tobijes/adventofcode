7dafc3e526789b4725c838742942c272779f5e19
Tobias Jessen <mail@tobijes.com>
2025-12-17 14:39:39 +0100

    Update 11-2.py

--- 2025/11/11-2.py
+++ 2025/11/11-2.py
@@ -1,5 +1,4 @@
-# Load data
-
+from collections import deque
 import sys
 
 is_test = False
@@ -26,3 +25,35 @@ def print_matrix(matrix):
         print()
 
 # Problem solution
+tree: dict[str, list[str]] = {"out": []}
+for d in data:
+    source, outputs = d.split(":")
+    outputs = outputs.strip().split(" ")
+    print(source, outputs)
+    tree[source] = outputs
+
+
+def number_of_paths(node, target, has_fft, has_dac, cache):
+    if (node, has_fft, has_dac) in cache:
+        print("Cache hit")
+        return cache[(node, has_fft, has_dac)]
+    
+    if node == target:
+        return 1 if has_dac and has_fft else 0
+    
+    has_fft = has_fft | (node == "fft")
+    has_dac = has_dac | (node == "dac")
+    result = 0
+ 
+    for next in tree[node]:
+        result += number_of_paths(next, target, has_fft, has_dac, cache)
+    
+    cache[(node, has_fft, has_dac)] = result
+    return result
+
+cache = {}
+score = number_of_paths("svr", "out", False, False, cache)
+print(cache)
+
+# score = 0
+print(f"{score=}")
